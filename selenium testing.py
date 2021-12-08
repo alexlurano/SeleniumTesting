ce8d9c19cf1b0016cffe0518cb32faa5069cb981
alexlurano <agerhards@comcast.net>
2021-12-07 20:28:01 -0700

    link testing

--- selenium testing.py
+++ selenium testing.py
@@ -1,61 +1,49 @@
 # import webdriver
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 # create webdriver object
+
+def find_top_tweet():
+    driver.get("https://trends24.in/united-states/")
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[3]/div/div[1]/ol")))
+    best_tweet = driver.find_element_by_xpath(("//div[@class='trend-card']"))
+    top_tweet_now = best_tweet.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[1]/ol/li[1]/a")
+    print("ta da" + top_tweet_now.text)
+    return top_tweet_now.text
+
+def make_a_meme():
+    driver.get("https://imgflip.com/memegenerator/27596988/Free")
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "mm-select-popular")))
+    popular_stuff = driver.find_element_by_id("mm-select-popular")
+    for dank in popular_stuff.find_elements_by_class_name('im'):
+        print(dank.get_attribute('title'))
+    return
+
+def find_that_link():
+    driver.get("https://www.google.com/search?q=arf&rlz=1C1CHBF_enUS862US862&oq=arf&aqs=chrome..69i57j0i433i512j0i512j0i10i512j0i433i512l3j0i512j0i433i512l2.1453j0j9&sourceid=chrome&ie=UTF-8")
+    #WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[3]/div/div[1]/ol")))
+    time.sleep(3)
+    best_tweet = driver.find_element_by_tag_name('h3')
+    best_tweet.click()
+    print("ta da" + top_tweet_now.text)
+    return top_tweet_now.text
+
+#----------------------------------
+#----------------------------------
+#----------------------------------
+#----------------------------------
 driver = webdriver.Chrome()
+#tweet_txt = find_top_tweet()
+#make_a_meme()
 
-# point at current environment - change this for Dev / Prod
-driver.get("localhost")
-# set the timeout for page loading at 30 seconds
-driver.set_page_load_timeout(30)
-
-# custom login for bot - change this to match login info
-user_name_input = driver.find_element_by_name("Username")
-user_name_input.clear()
-
-user_name_input.send_keys("botusername")
-user_name_input = driver.find_element_by_name("Username")
-user_name_input.clear()
-
-#navigate to claim
-driver.get("localhost/yadayada(claimid)")
-patient_name = driver.find_element_by_name("patient name")
-if (patient_name != currentclaim.name)
-    print("claim currentclaim.id name mismatch please verify")
-    #proceed to next claim in list
-
-# if the name matches the expected name, proceed to stay screen
-stay_link = driver.find_element_by_name("Stay") # todo -check metadata to find actual name of this element
-
-action = ActionChains(driver)
-
-action.click(on_element = stay_link)
-
-action.preform()
-driver.implicitly_wait(30)
-
-#once stay screen fully loads create CAR log based on closure type.
-create_stay_log = driver.find_element_by_name("Stay Log")
-create_stay_log.click()
-wait = WebDriverWait(driver, 30)
-create_stay_log = wait.until(EC.presence_of_element_located("check metadata of pop up for unique elements") #TODO
-staylog_type = driver.find_element_by_name("log type")
-staylog_type.select_by_visible_text("CAR")
-staylog_message = driver.find_element_by_name("title")
-staylog_message.send_keys("CANNED CLOSE MESSAGE")
-staylog_submit = driver.find_element_by_name("Submit")
-staylog_submit.click()
-driver.implicitly_wait(30)
+find_that_link() 
+driver.close()
 
 
-#close claim using closure reason
-claim_close = driver.find_element_by_name("close claim")
-claim_close.click()
 
-#todo - popup menu click for yes/no
 
-close_reason = driver.find_element_by_name("close reason")
-close_reason.select_by_visible_text("close reason goes here")
-# TODO handling for the second drop down of claim closure
-# TODO write a note for close note
-# TODO navigate back to claim screen and confirm claim has been closed
+
